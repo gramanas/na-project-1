4c13dffe6589ea36e921a42e5f7b3ea081c9e31a
gramanas <anastasis.gramm2@gmail.com>
2016-12-20 16:17:21 +0200

    voulia!!

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -11,11 +11,17 @@ t = arange(0,3.001,step=.001)
 def f(t):
   return (14*t)*(e**(t-2)) - 12*(e**(t-2)) - 7*(t**3) + 20*(t**2) - 26*t + 12
 
-# plt.xlabel("x")
-# plt.ylabel("f(x)")
-# plt.plot(t, f(t))
-# plt.grid(True)
-# plt.show()
+def f_der(t):
+  return (14*t)*(e**(t-2)) + 2*(e**(t-2)) - 21*(t**2) + 40*t - 26
+
+def f_der_2(t):
+  return (14*t)*(e**(t-2)) - 16*(e**(t-2)) - 42*t + 40
+
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.plot(t, f(t))
+plt.grid(True)
+plt.show()
 
 def bisect(a, b):
   a_1, b_1 = a, b
@@ -33,30 +39,6 @@ def bisect(a, b):
 
   return root, N, a_1, b_1
 
-print("\n++++ Bisection ++++\n")
-
-root, loops, a, b = bisect(0,1.5)
-print("Root in [%.2f,%.2f] after %d loops: f(%.6f) = %.6f"
-      % (a, b, loops, root, f(root)))
-
-root, loops, a, b = bisect(1.5,3)
-print("Root in [%.2f,%.2f] after %d loops: f(%.6f) = %.6f"
-      % (a, b, loops, root, f(root)))
-
-def f_der(t):
-  return (14*t)*(e**(t-2)) + 2*(e**(t-2)) - 21*(t**2) + 40*t - 26
-
-def f_der_2(t):
-  return (14*t)*(e**(t-2)) - 16*(e**(t-2)) - 42*t + 40
-
-# plt.xlabel("x")
-# plt.ylabel("f'(x)")
-plt.plot(t, f(t), c='r')
-#plt.plot(t, f_der(t), c='g')
-#plt.plot(t, f_der_2(t), c='b')
-plt.grid(True)
-plt.show()
-
 def new_raph(start):
   temp_l = [start, start - (f(start)/f_der(start))]
 
@@ -68,8 +50,40 @@ def new_raph(start):
 
   root = temp_l[N] 
 
-  return root, N, start
+  #N-1 because the N=N+1 happens at the very end of the while loop
+  return root, N - 1, start
 
+def intersection(a, b):
+  temp_l = [a, b, b - ((f(b)*(b - a))/f(b) - f(a))]
+
+  N = 1
+  while 0.0000005*abs(temp_l[N]) < abs(temp_l[N-1] - temp_l[N]):
+    temp =  temp_l[N] - (f(temp_l[N])*
+                         (temp_l[N] - temp_l[N-1]))/(f(temp_l[N]) - f(temp_l[N-1]))
+    temp_l.append(temp)
+    N = N + 1
+
+  root = temp_l[-1]
+
+  #N-1 for the same reason as the N-R method
+  return root, N - 1, a, b 
+
+#############
+# Bisection #
+#############
+print("\n++++ Bisection ++++\n")
+
+root, loops, a, b = bisect(0,1.5)
+print("Root in [%.2f,%.2f] after %d loops: f(%.6f) = %.6f\n"
+      % (a, b, loops, root, f(root)))
+
+root, loops, a, b = bisect(1.5,3)
+print("Root in [%.2f,%.2f] after %d loops: f(%.6f) = %.6f"
+      % (a, b, loops, root, f(root)))
+
+####################
+# Newton - Raphson #
+####################
 print("\n++++ Newton - Raphson ++++\n")
 
 root, loops, start = new_raph(1)
@@ -80,21 +94,15 @@ root, loops, start = new_raph(3)
 print("Starting at %.2f:\nafter %d iterations the root is: f(%.6f) = %.6f"
       % (start, loops, root, f(root)))
 
-def intersection(a, b):
-  temp_l = [a, b, b - ((f(b)*(b - a))/f(b) - f(a))]
+#################
+# Interpolation #
+#################
+print("\n++++ Interpolation ++++\n")
 
-  N = 1
-  while 0.0000005*abs(temp_l[N]) < abs(temp_l[N-1] - temp_l[N]):
-  #while f(temp_l[-1]) != 0.000000:
-#    if f(temp_l[N]) == f(temp_l[N-1]):
-#      break
-    temp =  temp_l[N] - (f(temp_l[N])*
-                         (temp_l[N] - temp_l[N-1]))/(f(temp_l[N]) - f(temp_l[N-1]))
-    temp_l.append(temp)
-#   print(N, temp_l[-1])
-    N = N + 1
+root, loops, a, b = intersection(0.7,0.9)
+print("Root found in [%.2f,%.2f] after %d iterations:" % (a, b, loops))
+print("f(%.6f) = %.6f\n" % (root, f(root)))
 
-  root = temp_l[-1]
-  return root, N, a, b 
-
-print(intersection(0,3))
+root, loops, a, b = intersection(1.7,2.1)
+print("Root found in [%.2f,%.2f] after %d iterations:" % (a, b, loops))
+print("f(%.6f) = %.6f" % (root, f(root)))
